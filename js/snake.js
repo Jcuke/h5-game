65ed5df0804077dd8b7ad2ca4d642586c553a20a
Jcuke <937000078@qq.com>
2017-06-11 11:11:05 +0800

    judet if colled yourself

--- js/snake.js
+++ js/snake.js
@@ -1,42 +1,30 @@
 $ = function (id) {
     return document.getElementById(id);
 };
-$v = $.value;
 
     var bc = 20;//小格子边长
-    var foodX = 0;
-    var foodY = 0;
-    var xdir = 0;
-    var ydir = 0;
-    var x = 20;
-    var y = 20;
-    var c = $('c');
-    var ctx = c.getContext('2d');
-    var kcode = {'w': 87, 's': 83, 'a': 65, 'd': 68};
+    var foodX = 0;//随即生成的食物的x坐标
+    var foodY = 0;//随即生成的食物的y坐标
+    var xdir = 0;//蛇移动的X方向,-1表是左,1表右,0表是方向不变
+    var ydir = 0;//蛇移动的Y方向,-1表是上,1表下,0表是方向不变
+    var x = 20;//蛇起始节点的X坐标
+    var y = 20;//蛇起始节点的Y坐标
+    var c = $('c');//获取画布对象
+    var ctx = c.getContext('2d');//获取2d画笔对象
+    var kcode = {'w': 87, 's': 83, 'a': 65, 'd': 68};//上下左右的键码
     var griddata = [];
     var currentActiveNodes = [];
 
-    var node = function(x,y,color){
+    var nodeJson = function(x,y,color){
         this.x = x;
         this.y = y;
         this.color = color;
     };
 
-    function refreshCurrentActiveNodes(){
-
-        for(i in currentActiveNodes){
-            var node = currentActiveNodes[i];
-            if(!node.color){
-                ctx.clearRect(node.x, node.y, bc, bc);
-            }else{
-                ctx.rect(node.x, node.y, bc, bc);
-            }
-        }
-
-
-    }
-
     function drawGrid() {
+        //ctx.strokeStyle="#0000ff";
+        //ctx.strokeRect(0,0,400,400);
+        //return;
         //画横线
         for (var i = 0; i <= c.width; i = i + bc) {
             ctx.strokeStyle = 'gray';
@@ -63,11 +51,12 @@ $v = $.value;
     function initGridData() {
         for (var i = 0; i < c.width / bc; i++) {
             for (var k = 0; k < c.height / bc; k++) {
-                var node = {'x': i * bc, 'y': k * bc, 'color': ''};
+                var node = new nodeJson(i * bc,k * bc, '');
                 griddata.push(node);
             }
         }
         genFood(true);
+        currentActiveNodes.unshift(new nodeJson(x, y, "red"));
     }
 
     initGridData();
@@ -78,7 +67,13 @@ $v = $.value;
     }();
 
     function dw() {
-        ctx.fillStyle = 'red';
+        //ctx.fillStyle = 'red';
+        ctx.fillStyle = '#' +
+            (function(color){
+                return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])
+                && (color.length == 6) ?  color : arguments.callee(color);
+            })('');
+        //随机颜色
         for(i in currentActiveNodes){
             var node = currentActiveNodes[i];
             ctx.fillRect(node.x, node.y, bc, bc);
@@ -91,8 +86,7 @@ $v = $.value;
     function genFood(genFoodFlag) {
         if(!genFoodFlag) return;
         ctx.fillStyle = 'red';
-        var i9 = true;
-        while (i9) {
+        while (genFoodFlag) {
             var x = randomNum(0, 19) * bc;
             var y = randomNum(0, 19) * bc;
             for (i in griddata) {
@@ -102,43 +96,44 @@ $v = $.value;
                         foodX = x;
                         foodY = y;
                         ctx.fillRect(foodX, foodY, bc, bc);
-                        i9 = false;
+                        genFoodFlag = !genFoodFlag;
                     }
                 }
             }
         }
-        genFoodFlag = false;
     }
-    currentActiveNodes.unshift(new node(x, y, "red"));
+
     !function () {
         x += bc * xdir;
         y += bc * ydir;
 
-        if(x > c.width || x < 0 || y > c.height || y < 0){
-            alert('Game Over');
-            return;
+        //撞到自已的节点
+        for(xi in currentActiveNodes){
+            if(currentActiveNodes.length > 1 && currentActiveNodes[xi].x === x && currentActiveNodes[xi].y === y){
+                alert('Collided with your self, Game Over');
+                return;
+            }
         }
 
-        currentActiveNodes.unshift(new node(x, y, ""));
+        currentActiveNodes.unshift(new nodeJson(x, y, ""));
         var tailNode = currentActiveNodes.pop();
         ctx.fillStyle = 'white';
         ctx.fillRect(tailNode.x, tailNode.y, bc, bc);
 
+        //撞到边框
+        if(currentActiveNodes[0].x > c.width || currentActiveNodes[0].x < 0 || currentActiveNodes[0].y > c.height || currentActiveNodes[0].y < 0){
+            alert('Collied boundary, Game Over');
+            return;
+        }
 
         if(x === foodX && y === foodY){
             genFood(true);
             x += bc * xdir;
             y += bc * ydir;
-            currentActiveNodes.unshift(new node(x, y, "red"));
+            currentActiveNodes.unshift(new nodeJson(x, y, "red"));
         }
 
-
-        //refreshCurrentActiveNodes();
         dw();
-
-//        if()
-//        nodes.unshift({'x':x, 'y':y, 'color': color});
-
 
         document.onkeyup = function (ev) {
             switch (ev.keyCode) {
@@ -160,6 +155,5 @@ $v = $.value;
                     break;
             }
         };
-
-        setTimeout(arguments.callee, 100);
+        setTimeout(arguments.callee, 150);
     }();
